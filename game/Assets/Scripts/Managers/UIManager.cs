0e279b5cd86586b092bc5f74bdc80ff2cc5ccf2c
John Luc Cubos <johnluccubos01@gmail.com>
2026-02-19 14:39:07 -0800

    UI Manager
    Manages the UI components in-game for ease of transitions

---
+++ game/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class UIManager : SingletonConstructor<UIManager>
+{
+    private void Awake()
+    {
+        ConstructSingleton(this); // ! DO NOT DELETE
+    }
+
+    /// Function: SwitchUI
+    /// <summary>
+    /// Purpose: Swaps one UI to another
+    /// </summary>
+    /// <returns> Nothing </returns>
+    /// <remarks>Note:</remarks>
+    void SwitchUI()
+    {
+        
+    }
+
+    /// Function: DisableUIs
+    /// <summary>
+    /// Purpose: Hides the UI from the player
+    /// </summary>
+    /// <returns> Nothing </returns>
+    /// <remarks>Note: </remarks>
+    void DisableUIs()
+    {
+        
+    }
+    /// Function: EnableUIs
+    /// <summary>
+    /// Purpose: Shows UI to the player
+    /// </summary>
+    /// <returns> Nothing </returns>
+    /// <remarks>Note:</remarks>
+    void EnableUIs()
+    {
+        
+    }
+}
