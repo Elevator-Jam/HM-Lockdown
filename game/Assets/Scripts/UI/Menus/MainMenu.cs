2c30de7c182010a8afaf47b336cbf7304571b366
shawndumdum <shawn.dumdum@yahoo.com>
2026-03-29 21:17:31 -0700

    Created .cs files for UI menus
    - MainMenu.cs created
    - PauseMenu.cs created
    - PostGame.cs created
    - Settings.cs created
    - Empty functions created inside all new .cs files

---
+++ game/Assets/Scripts/UI/Menus/MainMenu.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Loads main scene when the "play" button is clicked
+    public void OnClickPlay()
+    {
+        // Load main scene
+    }
+
+    // Opens settings menu when the "settings" button is clicked
+    public void OnClickSettings()
+    {
+        // Open settings menu
+    }
+
+    // Quits game when the "exit" button is clicked
+    public void OnClickExit()
+    {
+        // Quit game
+    }
+
+    // Opens a menu that displays credits when the "credits" button is clicked
+    public void OnClickCredits()
+    {
+        // Opens credits menu
+    }
+
+    // Closes credits menu when "close credits" button is clicked
+    public void OnClickCloseCredits()
+    {
+        // Close credits menu
+    }
+}
