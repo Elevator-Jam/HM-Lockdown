2194e460813d8adf42ae832cebf4c5b6aaa87af8
BrockPaperScissors <michaeljbrock91@gmail.com>
2026-04-07 22:27:13 -0700

    Adding fireball animation to bullet prefab

--- game/Assets/Scripts/Gameplay/Turret/BaseBullet.cs
+++ game/Assets/Scripts/Gameplay/Turret/BaseBullet.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BaseBullet : MonoBehaviour
 {
     [SerializeField] int damage;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag != "Entity")
