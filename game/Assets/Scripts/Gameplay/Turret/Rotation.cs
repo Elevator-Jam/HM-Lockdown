54b8d9d8776ee2ac048e9b32e18cf9ecc23b8909
Carlos Macias <carlos.macias23@gmail.com>
2026-04-08 20:30:04 -0700

    add new design main turret. change how it shoots

--- game/Assets/Scripts/Gameplay/Turret/Rotation.cs
+++ game/Assets/Scripts/Gameplay/Turret/Rotation.cs
@@ -1,9 +1,13 @@
+using System.Linq;
 using UnityEngine;
 
 public class Rotation : MonoBehaviour, IRotate
 {
     [SerializeField] Transform fireAxis;
     [SerializeField] float rotationSpeed;
+
+    [SerializeField] 
+    private SpriteRenderer[] spriteRenderers;
 
     public void Rotate(Transform target)
     {
@@ -15,9 +19,45 @@ public class Rotation : MonoBehaviour, IRotate
         Vector3 directionToTarget = target.position - fireAxis.position;
 
         float angleInRadians = Mathf.Atan2(directionToTarget.y, directionToTarget.x);
+        Debug.Log($"angle rads: {angleInRadians}");
         float angleInDegrees = angleInRadians * Mathf.Rad2Deg;
-        Quaternion targetRotation = Quaternion.Euler(0f, 0f, angleInDegrees);
+        Debug.Log($"angle: {angleInDegrees}");
+        //Quaternion targetRotation = Quaternion.Euler(0f, 0f, angleInDegrees);
+        //fireAxis.rotation = Quaternion.RotateTowards(fireAxis.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+        ShowTurretBasedOnAngle(angleInDegrees);
+    }
+    private void ShowTurretBasedOnAngle(float angleInDegrees)
+    {
+        int turrAnimIdx = 0;
+        if (angleInDegrees < 0f && angleInDegrees >= -20f)
+        {
+            turrAnimIdx = 0;
+            
+        } else if (angleInDegrees < -20f && angleInDegrees >= -80f)
+        {
+            turrAnimIdx = 1;
+        } else if (angleInDegrees < -80f && angleInDegrees >= -100f)
+        {
+            turrAnimIdx = 2;
 
-        fireAxis.rotation = Quaternion.RotateTowards(fireAxis.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+        } else if (angleInDegrees < -100f && angleInDegrees >= -140f)
+        {
+            turrAnimIdx = 3;
+
+        } else if (angleInDegrees < -140f && angleInDegrees >= -180f)
+        {
+            turrAnimIdx = 4;
+        }
+        BaseFiring.SetFirePointIdx(turrAnimIdx);
+        for (int i = 0; i < spriteRenderers.Count(); ++i)
+        {
+            if (i == turrAnimIdx)
+            {
+                spriteRenderers[i].enabled = true;
+            } else
+            {
+                spriteRenderers[i].enabled = false;
+            }
+        }
     }
 }
