cf63afd4a3f65301e8f954c7ac0baa7c47639d5b
Carlos Macias <carlos.macias23@gmail.com>
2026-04-10 05:38:52 -0700

    add new secondary turrets

--- game/Assets/Scripts/UI/SelectTurret.cs
+++ game/Assets/Scripts/UI/SelectTurret.cs
@@ -1,12 +1,54 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectTurret : MonoBehaviour
 {
+    private static SelectTurret currentSelection;
+
     [SerializeField] GameObject turretPrefab;
     [SerializeField] int turretValue;
+    
+    [Header("Visual Feedback")]
+    [SerializeField] Color normalColor = Color.white;
+    [SerializeField] Color selectedColor = Color.green;
+    
+    private Image buttonImage;
+
+    private void Awake()
+    {
+        buttonImage = GetComponent<Image>();
+        if (buttonImage != null) buttonImage.color = normalColor;
+    }
+
     public void OnClick()
     {
+        Debug.Log("calling select turret. onclick");
+        // If we click the same button again, deselect it
+        if (currentSelection == this)
+        {
+            Deselect();
+            BuildingManager.Instance.SetTurretSelected(null);
+            BuildingManager.Instance.SetTurretValue(0);
+            return;
+        }
+
+        // Deselect the previous button
+        if (currentSelection != null)
+        {
+            currentSelection.Deselect();
+        }
+
+        // Select this button
+        currentSelection = this;
+        if (buttonImage != null) buttonImage.color = selectedColor;
+
         BuildingManager.Instance.SetTurretSelected(turretPrefab);
         BuildingManager.Instance.SetTurretValue(turretValue);
+    }
+
+    public void Deselect()
+    {
+        if (buttonImage != null) buttonImage.color = normalColor;
+        if (currentSelection == this) currentSelection = null;
     }
 }
