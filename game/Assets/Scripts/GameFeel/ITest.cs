458dfdd844f8788509e0d865643074fc25e30847
Taro Omiya <japtar10101@duck.com>
2026-05-28 20:08:06 -0700

    Adding source and target fields in `ITest`

--- game/Assets/Scripts/GameFeel/ITest.cs
+++ game/Assets/Scripts/GameFeel/ITest.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 namespace HM.Lockdown.GameFeel {
     [System.Serializable]
     public abstract class ITest : /*UnityEngine.Object*/ ScriptableObject {
+        [SerializeField]
+        protected string source = "source";
+        [SerializeField]
+        protected string target = "target";
     }
 }
