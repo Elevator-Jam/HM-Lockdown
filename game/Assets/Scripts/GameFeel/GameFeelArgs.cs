923d3c02d275574ee7d9e7275c32d9857a0c351b
Taro Omiya <japtar10101@duck.com>
2026-05-29 20:23:47 -0700

    * Filling out `GameFeelArgs` -- which was originally stub code -- with proper implementation of dictionary library. * Also adding `RequiredArgsList`, a dictionary of string and class type pair to verify if `GameFeelArgs` contains the right arguments.

--- game/Assets/Scripts/GameFeel/GameFeelArgs.cs
+++ game/Assets/Scripts/GameFeel/GameFeelArgs.cs
@@ -1,7 +1,122 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace HM.Lockdown.GameFeel {
-    public class GameFeelArgs {
+    /// <summary>
+    /// Dictionary of arguments to feed <see cref="GameFeelEvent"/> and <see cref="GameFeelAction"/>.
+    /// </summary>
+    public class GameFeelArgs : IDictionary<string, object> {
+        private const int DEFAULT_STARTING_SIZE = 4;
+
+        private readonly Dictionary<string, object> arguments;
+
+        public GameFeelArgs(MonoBehaviour invoker, Transform source, int startingSize = DEFAULT_STARTING_SIZE) {
+            if (invoker != null) {
+                throw new System.ArgumentNullException(nameof(invoker));
+            }
+
+            // Set member variables
+            Invoker = invoker;
+            arguments = new(startingSize);
+            //arguments = new(startingSize) { {
+            //    GameFeelAction.DEFAULT_NAME_SOURCE, source
+            //} };
+        }
+
+        /// <summary>
+        /// Gets the initiator of event caller
+        /// </summary>
+        public MonoBehaviour Invoker {
+            get;
+        }
+
+        /// <summary>
+        /// Gets an argument
+        /// </summary>
+        public T Get<T>(string argName) {
+            return TryGetValue(argName, out T value) ? value : default;
+        }
+
+        /// <summary>
+        /// Attempts to get an argument.
+        /// </summary>
+        public bool TryGetValue<T>(string argName, out T argValue) {
+            // Check if the dictionary contains this argument
+            // Check if the value type is correct
+            if (arguments.TryGetValue(argName, out object baseValue) && (baseValue is T derivedValue)) {
+                argValue = derivedValue;
+                return true;
+            }
+
+            // Otherwise, return default
+            argValue = default;
+            return false;
+        }
+
+        #region Implementing IDictionary<string, object>
+        /// <inheritdoc/>
+        public object this[string key] {
+            get => arguments[key];
+            set => arguments[key] = value;
+        }
+
+        /// <inheritdoc/>
+        public ICollection<string> Keys => arguments.Keys;
+
+        /// <inheritdoc/>
+        public ICollection<object> Values => arguments.Values;
+
+        /// <inheritdoc/>
+        public int Count => arguments.Count;
+
+        /// <inheritdoc/>
+        public bool IsReadOnly => ((ICollection<KeyValuePair<string, object>>)arguments).IsReadOnly;
+
+        /// <inheritdoc/>
+        public void Add(string key, object value) => arguments.Add(key, value);
+
+        /// <inheritdoc/>
+        public bool Remove(string key) => arguments.Remove(key);
+
+        /// <inheritdoc/>
+        public bool ContainsKey(string key) => arguments.ContainsKey(key);
+
+        /// <inheritdoc/>
+        public bool TryGetValue(string key, out object value) => arguments.TryGetValue(key, out value);
+
+        /// <inheritdoc/>
+        public void Clear() => arguments.Clear();
+
+        /// <inheritdoc/>
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator() {
+            return ((IEnumerable<KeyValuePair<string, object>>)arguments).GetEnumerator();
+        }
+
+        /// <inheritdoc/>
+        void ICollection<KeyValuePair<string, object>>.Add(KeyValuePair<string, object> item) {
+            ((ICollection<KeyValuePair<string, object>>)arguments).Add(item);
+        }
+
+        /// <inheritdoc/>
+        bool ICollection<KeyValuePair<string, object>>.Contains(KeyValuePair<string, object> item) {
+            return ((ICollection<KeyValuePair<string, object>>)arguments).Contains(item);
+        }
+
+        /// <inheritdoc/>
+        void ICollection<KeyValuePair<string, object>>.CopyTo(KeyValuePair<string, object>[] array, int arrayIndex) {
+            ((ICollection<KeyValuePair<string, object>>)arguments).CopyTo(array, arrayIndex);
+        }
+
+        /// <inheritdoc/>
+        bool ICollection<KeyValuePair<string, object>>.Remove(KeyValuePair<string, object> item) {
+            return ((ICollection<KeyValuePair<string, object>>)arguments).Remove(item);
+        }
+
+        /// <inheritdoc/>
+        IEnumerator IEnumerable.GetEnumerator() {
+            return ((IEnumerable)arguments).GetEnumerator();
+        }
+        #endregion
     }
 }
---
+++ game/Assets/Scripts/GameFeel/RequiredArgsList.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HM.Lockdown.GameFeel {
+    /// <summary>
+    /// Helper class indicating what argument names and types are expected
+    /// </summary>
+    public class RequiredArgsList : IReadOnlyDictionary<string, Type> {
+        private const int DEFAULT_STARTING_SIZE = 4;
+        private readonly Dictionary<string, Type> requiredArguments;
+
+        public RequiredArgsList(int startingSize = DEFAULT_STARTING_SIZE) {
+            requiredArguments = new(startingSize);
+        }
+
+        /// <summary>
+        /// Make sure that <paramref name="args"/> contains all the required arguments and the appropriate type.
+        /// </summary>
+        public bool VerifyArgs(GameFeelArgs args) {
+            bool toReturn = true;
+
+            // Go through all required arguments
+            foreach (var (argName, argType) in requiredArguments) {
+                // Check if the args parameter has key
+                if (!args.TryGetValue(argName, out object testValue)) {
+                    Debug.LogWarningFormat("{0}: {1} doesn't contain required argument, \"{2}\"",
+                        nameof(RequiredArgsList), nameof(GameFeelArgs), argName);
+                    toReturn = false;
+                    continue;
+                }
+
+                // Check if the arg type is correct
+                if (!argType.IsInstanceOfType(testValue)) {
+                    Debug.LogWarningFormat("{0}: {1} has the wrong value type for argument \"{2}\"; expected type \"{3}\"",
+                        nameof(RequiredArgsList), nameof(GameFeelArgs), argName, argType.Name);
+                    toReturn = false;
+                    continue;
+                }
+            }
+
+            return toReturn;
+        }
+
+        #region Implementing IReadOnlyDictionary<string, Type>
+        /// <inheritdoc/>
+        public Type this[string key] => requiredArguments[key];
+
+        /// <inheritdoc/>
+        public IEnumerable<string> Keys => requiredArguments.Keys;
+
+        /// <inheritdoc/>
+        public IEnumerable<Type> Values => requiredArguments.Values;
+
+        /// <inheritdoc/>
+        public int Count => requiredArguments.Count;
+
+        /// <inheritdoc/>
+        public bool ContainsKey(string key) => requiredArguments.ContainsKey(key);
+
+        /// <inheritdoc/>
+        public bool TryGetValue(string key, out Type value) => requiredArguments.TryGetValue(key, out value);
+
+        /// <inheritdoc/>
+        public IEnumerator<KeyValuePair<string, Type>> GetEnumerator() {
+            return ((IEnumerable<KeyValuePair<string, Type>>)requiredArguments).GetEnumerator();
+        }
+
+        /// <inheritdoc/>
+        IEnumerator IEnumerable.GetEnumerator() {
+            return ((IEnumerable)requiredArguments).GetEnumerator();
+        }
+        #endregion
+
+        /// <summary>
+        /// Adds an argument and type to this dictionary
+        /// </summary>
+        internal void AddRequiredArg<ArgType>(string argName, string scriptName = null) {
+            // Make sure the arguments are valid
+            if (string.IsNullOrWhiteSpace(argName)) {
+                throw new ArgumentNullException(nameof(argName));
+            }
+
+            // Add the argument to the required arguments dictionary
+            Type type = typeof(ArgType);
+            if (requiredArguments.TryAdd(argName, type)) {
+                // If no conflicts, add key and type, and halt
+                return;
+            } else if (requiredArguments[argName] == type) {
+                // If there is a conflict, check to see if the arg type is the same
+                // halt if they are
+                return;
+            }
+
+            // Indicate the conflict
+            if (string.IsNullOrEmpty(scriptName)) {
+                Debug.LogErrorFormat("{0}: Conflicting argument name \"{1}\" detected", nameof(RequiredArgsList), argName);
+            } else {
+                Debug.LogErrorFormat("{0}: Script \"{1}\" attempted to add a conflicting argument name, \"{2}\"", nameof(RequiredArgsList), scriptName, argName);
+            }
+        }
+    }
+}
