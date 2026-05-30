8678f16ea9927a0b287c82e8b31c4c8e5da349f2
Taro Omiya <japtar10101@duck.com>
2026-05-29 21:02:09 -0700

    Adding stub code for various singletons that I may or may not use throughout this branch.

---
+++ game/Assets/Scripts/Managers/PauseManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HM.Lockdown.Managers {
+    public class PauseManager : SingletonConstructor<PauseManager> {
+        private void Awake() {
+            ConstructSingleton(this); // ! DO NOT DELETE
+        }
+    }
+}
---
+++ game/Assets/Scripts/Managers/PoolManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HM.Lockdown.Managers {
+	public class PoolManager : SingletonConstructor<PoolManager> {
+        private void Awake() {
+            ConstructSingleton(this); // ! DO NOT DELETE
+        }
+    }
+}
